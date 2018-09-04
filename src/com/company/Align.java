ad659ecc314bbabc7b0c2c0a6de5e747ea6abcbe
renando <renan@tiket.com>
2018-09-04 12:18:31 +0700

    Commit GUI via intellij

---
+++ src/com/company/Align.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
+public class Align {
+
+  private JButton ok;
+  private JButton help;
+  private JButton cancel;
+  private JCheckBox snaptogrid;
+  private JCheckBox showgrid;
+  private JTextField textField1;
+  private JTextField textField2;
+
+  private void createUIComponents() {
+    // TODO: place custom component creation code here
+  }
+}
